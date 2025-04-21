71e0e74f97bde0104d5aaae7d8981055a422bb23
Efrain1013 <efrain1013@gmail.com>
2025-04-21 17:38:05 -0400

    nothing really

--- bandtool_update/main.py
+++ bandtool_update/main.py
@@ -5,9 +5,9 @@ from bandtool.output_utils import build_band_extrema_list, build_total_results, 
 
 def main():
     print("Running main script")
-    all_kpoints, all_energies, NKPTS, NBANDS = find_ken_values("../../Test_Band_Si8/BAND.dat")
+    all_kpoints, all_energies, NKPTS, NBANDS = find_ken_values("BAND.dat")
     all_kpoints_norm, all_energies_split = split_kE(all_kpoints, all_energies, NKPTS, NBANDS)
-    kpoint_values, kpoint_labels = get_kpoint_labels("../../Test_Band_Si8/KLABELS")
+    kpoint_values, kpoint_labels = get_kpoint_labels("KLABELS")
     all_energies_norm, val_index, NDEG_VAL, con_index, NDEG_CON, BAND_GAP = norm_energies(all_energies_split)
     plot_bands(all_kpoints_norm, all_energies_norm, kpoint_values, kpoint_labels, NKPTS)
     cbm_results, vbm_results = get_band_results(all_kpoints_norm, all_energies_norm, kpoint_values, kpoint_labels, con_index, val_index, NDEG_CON+1, NDEG_VAL+1, NKPTS)

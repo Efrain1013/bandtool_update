9f69c5aa96f71a9ee2af749993c49e6e7e189056
Efrain1013 <efrain1013@gmail.com>
2025-05-01 00:54:52 -0400

    Starting from bandtool Version 0.1.1 Now

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# My Project
+<<<<<<< HEAD
+# Ind_Study
+Any code that I have or work in my independent study class is going to be here so I can make changes and import the codes to whatever device I'm using.
+
+Most of the code is related to post-processing off VASP calculations. As of now, I use vaspkit.1.5.0 to process the VASP files, and then use my code to read in the data from vaspkit. With these codes we calculate the effective mass and plot the band structure.
+=======
+# BandTool
+
+**BandTool** is a Python package for analyzing electronic band structure data, identifying extrema, calculating effective masses, and generating plots.
+
+## Installation
+
+You can install the package directly from GitHub:
+
+```bash
+pip install git+https://github.com/Efrain1013/bandtool.git
+```
+
+And you can update it from GitHub with:
+```bash
+pip install --upgrade git+https://github.com/Efrain1013/bandtool.git
+```
+
+>>>>>>> 0c2ee5f (adding files from terminal on mac)
--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='bandtool_update',
-    version='0.1',
+    name="bandtool",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'scipy',
-        'matplotlib'
+        "numpy",
+        "matplotlib",
+        # add any other dependencies here
     ],
     entry_points={
-        'console_scripts': [
-            'bandtool_update=bandtool_update.main:main',
+        "console_scripts": [
+            "bandtool=bandtool.main:main",  # or whatever the entry point is
         ],
     },
+    author="Efrain Martinez",
+    author_email="efrain1013@gmail.com",
+    description="A tool for analyzing electronic band structure data.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Efrain1013/bandtool",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
 )
-
